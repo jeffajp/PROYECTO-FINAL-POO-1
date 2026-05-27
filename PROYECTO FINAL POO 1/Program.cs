6a7fc90df8892a2cf4d4a22dcd751d6fd4e4a59f
HP <HP@DESKTOP-6S1BT0A>
2026-05-26 23:57:08 -0600

    Agregar archivos de proyecto.

---
+++ PROYECTO FINAL POO 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_FINAL_POO_1 //VENTA DE COMIDA DOÑA SHENI 
+{
+	public class comida
+	{
+		private string cliente;
+		private int codigo;
+
+	}
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
